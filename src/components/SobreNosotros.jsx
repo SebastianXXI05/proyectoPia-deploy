d66f5276405d2b7bd579eec498f43af2d6e9533d
Alejandro <junior@gmail.com>
2022-10-31 13:36:06 -0500

    css sobre nosotros

--- src/components/SobreNosotros.jsx
+++ src/components/SobreNosotros.jsx
@@ -10,13 +10,13 @@ function SobreNosotros() {
                 </p>
               </div>
               <div className={styles.espacio}>
-                <h1 className={styles.title}>Acerca de nuestra misión</h1>
+                <h2 className={styles.title}>Acerca de nuestra misión</h2>
                 <p>Nuestra misión es brindar a los usuarios información relevante acerca de lo que tiene la comuna 13 para ofrecerte. Lograremos ser los más grandes proveedores de información relaciona con la comuna 13. </p>
               </div>
               <div className={styles.espacio}>
-                <h1 className={styles.title}>¿Qué hacemos?</h1>
-                <h2 className={styles.intro}>Introducción</h2>
-                <h3>CM 13 aspira a que esta comuna de Medellín sea reconocida como destino turistico de gran importancia para la humanidad.</h3>
+                <h2 className={styles.title}>¿Qué hacemos?</h2>
+                <h3 className={styles.intro}>Introducción</h3>
+                <h4>CM 13 aspira a que esta comuna de Medellín sea reconocida como destino turistico de gran importancia para la humanidad.</h4>
                 <p>Nuestra página web (<a href='#'>WWW.conocelacomuna13.com</a>) es una fuente importante dedicada a brindar noticias, opiniones y análisis con información confiable acerca de los destino turisticos que este lugar puede ofrecerte.
                 </p>
                 <p>Nuestro principal compromiso es con los comerciantes de los tours, restaurantes y hoteles de la comuna 13, nos hemos comprometido a ser la fuente que brinde la información más importante acerca de los beneficios y gratas experiencias que podrás obtener. </p>
@@ -25,17 +25,17 @@ function SobreNosotros() {
                 </p>
               </div>
               <div className={styles.espacio}>
-                <h1 className={styles.title}>Objetivos estratégicos</h1>
+                <h2 className={styles.title}>Objetivos estratégicos</h2>
                 <p>El objetivo de nuestra estrategia es promover el conocimiento acerca de la comuna 13 de Medellín que contribuye a que los futuros turistas de esta comuna puedan estar informados. Esperamos lograrlo facilitando el acceso a información útil y digna de confianza, así como apoyando la capacidad para entregar y mantener esta estrategía de integración.</p>
                 <p>Con nuestro nuevo enfoque estratégico comprometemos nuestras principales capacidades para informar a la comunidad que desea visitar la comuna 13 de Medellín.</p>
               </div>
               <div className={styles.espacio}>
-                <h1 className={styles.title}>Historia</h1>
+                <h2 className={styles.title}>Historia</h2>
                 <p>conoce la comuna 13 se originó como un proyecto diseñado por un equipo de estudiantes que tenían como objetivo lograr que se pudiera brindar información concreta y relevante acerca de la comuna 13.</p>
                 <p>La pániga Web con financiamiento clasificado, el proyecto empezó a mitad del año 2022 y será lanzada proximamente en un futuro indefinido. </p>
               </div>
               <div className={styles.espacio}>
-                <h1 className={styles.title}>Contactanos</h1>
+                <h2 className={styles.title}>Contactanos</h2>
                 <p>A continuación podras encontrar las redes sociales en las que podrdas informarte acerca de nuestra pagina web y la información más importante que podras encontrar</p>
               </div>
               <div>
@@ -47,7 +47,7 @@ function SobreNosotros() {
                 </ul>
               </div>
               <div>
-                <h3 className={styles.title}>contactanos de una manera más privada</h3>
+                <h2 className={styles.title}>contactanos de una manera más privada</h2>
                 <p>Mediante Whattsapp: **********</p>
                 <p>Mediante gmail: <a href="#"></a></p>
               </div>
